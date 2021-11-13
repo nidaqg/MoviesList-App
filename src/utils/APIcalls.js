bf3f54aca3c506795f8cf82a5e081c39f20d364d
nqg <nidaghuman@gmail.com>
2021-11-12 20:45:34 -0500

    added .env file for api key

--- src/utils/APIcalls.js
+++ src/utils/APIcalls.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import {API_KEY} from '@env';
 
 
 export const getMovies = (query) => {
-let baseURL = "https://api.themoviedb.org/3/search/movie?"
-let APIkey = "api_key=d7de7ce6389671074d6332dc2edaec14"
+let baseURL = "https://api.themoviedb.org/3/search/movie?api_key="
+let APIkey = API_KEY
 let extras = "&language=en-US&page=1&include_adult=false"
 
     return (
