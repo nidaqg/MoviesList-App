f15e1cc8f1ce5fbcc5ea42380ba072a26b27d47e
nqg <nidaghuman@gmail.com>
2021-12-07 12:01:02 -0500

    moved styles into own file in watchlist page

--- src/features/watchlist/screens/WatchListPage.js
+++ src/features/watchlist/screens/WatchListPage.js
@@ -1,23 +1,10 @@
 import React, {useContext } from "react";
-import styled from "styled-components/native";
 import { MoviesContainer } from "../../../components/styles";
 import { WatchListContext } from "../../../utils/context/WatchListContext";
 import { MovieDetailCard } from "../../../components/MovieDetailCard";
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View } from "react-native";
+import { Info,NoInfoCard } from "../styles";
 
-const NoInfoCard = styled(View)`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  padding: 20px;
-  padding-top: 50px;
-`;
-
-const Info = styled(Text)`
-  font-size: 24px;
-  font-family: Lato_400Regular;
-  color: white;
-`;
 
 export const WatchListPage = () => {
   const { watchList } = useContext(WatchListContext);
---
+++ src/features/watchlist/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const NoInfoCard = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  padding-top: 50px;
+`;
+
+export const Info = styled.Text`
+  font-size: 24px;
+  font-family: Lato_400Regular;
+  color: white;
+`;
